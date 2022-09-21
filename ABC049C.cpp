e24b612daa4fe94ef9c38af8794eebdfefd4657e
sukekyo000 <sukekyo000@gmail.com>
2022-09-21 19:32:09 +0900

    ABC049C Answer

--- ABC049C.cpp
+++ ABC049C.cpp
@@ -4,13 +4,38 @@
 using namespace std;
 
 int main() {
-    int i, j;
-
-    string S;
+    int i, j, words_num;
+    string S, words[4] = {"dream", "dreamer", "erase", "eraser"};
+    bool judge = true;
 
     cin >> S;
 
-    printf("%s\n", std::reverse(S.begin(), S.end()));
+    std::reverse(S.begin(), S.end());
+
+    for(i = 0; i < 4; i++){
+        std::reverse(words[i].begin(), words[i].end());
+    }
+
+    words_num = S.length();
+    for(i = 0; i < words_num;){    
+        for(j = 0; j < 4; j++){
+            if(S.substr(i, words[j].length()) == words[j]){
+                i += words[j].length();
+                break;
+            } else {
+                if(j == 3){
+                    judge = false;
+                    i = S.length();
+                }
+            }
+        }
+    }
+
+    if(judge == true){
+        cout << "YES" << endl;
+    } else {
+        cout << "NO" << endl;
+    }
 
     return 0;
 }
